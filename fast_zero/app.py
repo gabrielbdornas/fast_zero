53c984cbfe450e5f4dac64e910174457abf33892
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-01-24 16:42:30 -0300

    Add html endpoint and test

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,9 +2,24 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK)
+def read_root_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1>Olá Mundo</h1>
+      </body>
+    </html>"""
